f16a815c991a1ddd16630ba27b7ec5757a737f9f
vlam0 <96927718+vlam0@users.noreply.github.com>
2022-08-01 19:56:34 -0700

    update

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const enabledBorder = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(32.0),
+      borderSide: const BorderSide(
+        color: Colors.white,
+      )
+  );
