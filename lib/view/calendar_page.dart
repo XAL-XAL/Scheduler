79f760a201c385c4c51d04b2f643eeeebc56d08d
AlecADAS <aleca@dynamicarsolutions.com>
2022-08-01 19:20:38 -0700

    Added bottom Navigation Bar

---
+++ lib/view/calendar_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({Key? key}) : super(key: key);
+
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.blue.shade400,
+      title: Text("Calendar"),
+    ));
+  }
+}
---
+++ lib/view/directory_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DirectoryPage extends StatefulWidget {
+  const DirectoryPage({Key? key}) : super(key: key);
+
+  @override
+  State<DirectoryPage> createState() => _DirectoryPageState();
+}
+
+class _DirectoryPageState extends State<DirectoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      backgroundColor: Colors.blue.shade400,
+      title: Text("Directory"),
+    ));
+  }
+}
