b2dcfb78d80f57ab125c4360bc9cc3b6046fe74d
AlecADAS <aleca@dynamicarsolutions.com>
2022-08-02 12:31:27 -0700

    Added Calendar

--- lib/view/calendar_page.dart
+++ lib/view/calendar_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarPage extends StatefulWidget {
   const CalendarPage({Key? key}) : super(key: key);
@@ -9,12 +10,43 @@ class CalendarPage extends StatefulWidget {
 }
 
 class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.blue.shade400,
-      title: Text("Calendar"),
-    ));
+      appBar: AppBar(
+        title: Text('TableCalendar Example'),
+        centerTitle: true,
+        backgroundColor: Colors.blue.shade400,
+      ),
+      body: TableCalendar(
+        focusedDay: focusedDay,
+        firstDay: DateTime(1990),
+        lastDay: DateTime(2050),
+        calendarFormat: _calendarFormat,
+        onFormatChanged: (CalendarFormat _format) {
+          setState(() {
+            _calendarFormat = _format;
+          });
+        },
+
+        //Changing selected day
+        selectedDayPredicate: (DateTime date) {
+          return isSameDay(selectedDay, date);
+        },
+        onDaySelected: (DateTime selectDay, DateTime focusDay) {
+          setState(() {
+            selectedDay = selectDay;
+            focusedDay = focusDay;
+          });
+        },
+        headerStyle: HeaderStyle(
+          formatButtonShowsNext: false,
+        ),
+      ),
+    );
   }
 }
